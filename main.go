5e66585a0a501e7277c88a9cb0d4ce5b20c5b3de
ninomae makoto <ninomae.makoto.develop@gmail.com>
2019-12-29 22:22:52 +0900

    update: コード整理

--- main.go
+++ main.go
@@ -8,10 +8,6 @@ import (
 	// Imports the Google Cloud Storage client package.
 )
 
-// func init() {
-// 	http.HandleFunc("/", root)
-// }
-
 // [START func_root]
 func root(w http.ResponseWriter, r *http.Request) {
 
@@ -40,9 +36,6 @@ func root(w http.ResponseWriter, r *http.Request) {
 // [END func_root]
 
 func main() {
-	// http.Handle("/manifest.json", http.FileServer(http.Dir("dist")))
-	// http.Handle("/js", http.FileServer(http.Dir("dist")))
-	// http.Handle("/css", http.FileServer(http.Dir("dist")))
 
 	// log.Printf(os.Args[1])
 	http.HandleFunc("/", root)
@@ -59,29 +52,4 @@ func main() {
 	}
 	// [END setting_port]
 
-	// fmt.Printf("test")
-	// ctx := context.Background()
-
-	// Sets your Google Cloud Platform project ID.
-	// projectID := "sealion-list"
-
-	// Creates a client.
-	// client, err := storage.NewClient(ctx)
-	// if err != nil {
-	// 	log.Fatalf("Failed to create client: %v", err)
-	// }
-
-	// Sets the name for the new bucket.
-	// bucketName := "test"
-
-	// Creates a Bucket instance.
-	// bucket := client.Bucket(bucketName)
-	// fmt.Printf("BucketName %v created.\n", bucket)
-
-	// Creates the new bucket.
-	// if err := bucket.Create(ctx, projectID, nil); err != nil {
-	// 	log.Fatalf("Failed to create bucket: %v", err)
-	// }
-
-	// fmt.Printf("Bucket %v created.\n", bucketName)
 }
